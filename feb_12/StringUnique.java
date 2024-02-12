5496ad1ec02ce98a88155d2caaf5764e0f164f94
SONUKUMAR <sonukumarbksce1204@gmail.com>
2024-02-12 20:47:15 +0530

    Version 1

---
+++ feb_12/StringUnique.java
@@ -0,0 +1,24 @@
+package feb_12;
+public class StringUnique {
+    int uniquecheck(String str){
+        int f=0;
+        for(int i=0;i<str.length();i++){
+            for(int j=0;j<str.length();j++){
+                if((str.charAt(i))==((str.charAt(j)))&&(i!=j)){
+                    f=1;
+                }
+            }
+            if(f==0){
+                return i;
+            }
+        }
+        return -1;
+    }
+    public static void main(String[] args){
+        StringUnique obj=new StringUnique();
+        String s="ShSam";
+        int i=obj.uniquecheck(s);
+        System.out.println(i);
+    }
+    
+}
